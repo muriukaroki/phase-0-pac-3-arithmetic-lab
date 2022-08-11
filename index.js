416dbcff4136ff6c3dbffa424835a1d833ae94fe
Justin <justin.kjaroki@student.moringaschool.com>
2022-08-11 23:24:31 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,57 @@
+function add() {
+    return (1 + 80);
+}
+add();
+
+function subtract() {
+    return (60 - 40);
+}
+subtract();
+
+function multiply() {
+    return (2 * 3.4);
+}
+multiply();
+
+function divide() {
+    return (5.0 / 2.5)
+}
+divide();
+
+function add(a, b) {
+    return (a + b)
+}
+add(1, 80);
+
+function subtract(a, b) {
+    return (a - b);
+}
+subtract(60, 40);
+
+function multiply(a, b) {
+    return (a * b);
+}
+multiply(2, 3.4);
+
+function divide(a, b) {
+    return (a / b)
+}
+divide(5.0, 2.5);
+
+function increment(n) {
+    n++;
+    return n;
+}
+
+function decrement(n) {
+    n--;
+    return n;
+}
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n)
+} 
